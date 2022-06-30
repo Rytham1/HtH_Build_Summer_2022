b9a7d83518ed405de26bfff255532677006147f6
Admin <admin@Admins-iMac.local>
2022-06-30 11:21:50 -0700

    adding 'getting started with python' activity

---
+++ Getting-Started-with-Python-RythamD/about_me.py
@@ -0,0 +1,27 @@
+name = "Rytham Dawar"
+nickname = "Ryth"
+age = 17
+python_experience = True
+
+hobbies = [
+    "playing videogames", "Playing basketball", "playing soccer",
+    "Watching movies"
+]
+
+favs = {
+    "movie": "Friday",
+    "number": 7,
+    "food": "Burritos",
+    "Hobby": hobbies[0],
+    "Videogame": "Call of duty",
+    "Anime": "Black Clover",
+}
+
+print(name, nickname, age, python_experience)
+print("\n")
+print(hobbies)
+print("\n")
+print(favs)
+
+
+all_vars = dict(vars())
